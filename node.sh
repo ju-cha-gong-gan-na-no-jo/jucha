223f0197e30019e720b9de1eba41ddab0c593dd0
Debian <bitnami@ip-172-26-9-14.ap-northeast-2.compute.internal>
2022-03-21 03:26:37 +0000

    first commit

---
+++ node.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+if [ "$1" = "status" ]
+then
+	ps -ef | grep 'node'
+fi
+
+node=$(ps -ef | grep 'node app.js')
+first1=$(echo ${node} | cut -d " " -f2)
+first2=$(echo ${node} | cut -d " " -f8)
+
+if [ "$1" = "start" ]
+then
+	if [ $first2 == "node" ]
+	then
+		for var in $first1
+		do
+ 			if [ -n ${var} ]
+			then
+ 				result=$(kill -9 ${var})
+				echo "${var} process is killed."
+				nohup node app.js > /dev/null 2>&1 
+				echo "node is running."
+				ps -ef | grep 'node'
+			fi
+ 		done
+	 else
+ 		nohup node app.js > /dev/null 2>&1
+		echo "node is running."
+		ps -ef | grep 'node'
+ 	fi
+fi
+if [ "$1" = "stop" ]
+then
+	if [ $first2 == "node" ]
+	then
+		for var in $first1
+		do
+			if [ -n ${var} ]
+			then
+				result=$(kill -9 ${var})
+				echo "${var} process is killed."
+				ps -ef | grep 'node'
+			fi
+		done
+	else
+		echo running process not found.
+	fi
+fi
